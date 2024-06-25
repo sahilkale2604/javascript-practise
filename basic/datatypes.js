917c8fd4d76eadcf01ff7730ad54fb023efb9f7b
sahilkale2604 <157214195+sahilkale2604@users.noreply.github.com>
2024-06-25 07:30:29 +0000

    datatypes operations

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -11,3 +11,16 @@ let score = "50"
 console.log(typeof score);
 let valueinNumber = Number(score)
 console.log(typeof valueinNumber);
+
+// operations//
+// ffgdf
+// WebGLTransformFeedbackgf
+// dfg
+// d
+// gfd
+// gfdg use control + / to comment out multiple lines
+console.log((3+5));
+console.log(3+5);
+console.log("1"+2);
+console.log(1+"2");
+console.log((1+2+3+45+56));
