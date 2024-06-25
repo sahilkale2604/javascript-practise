89e7afbb21f0d94d75b0605243378e73cd027c83
sahilkale2604 <157214195+sahilkale2604@users.noreply.github.com>
2024-06-25 07:08:38 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,13 @@
+/*
+number=>2 to power 53
+bigint
+string =>""
+boolean =>true/false
+null =>standalone value
+underfined =>
+    symbol => unique
+*/
+let score = "50"
+console.log(typeof score);
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,11 @@
-console.log("sahil")
+const account = 1244556
+let accountemail = "sahilkale2642004@gmail.com"
+var accountpasssword = "23546"
+city = "jaipur"
+let accountname;
+
+console.log(account)
+/*comment 
+*/
+console.table([account,accountemail,accountpasssword,city,accountname])
+//erf//
