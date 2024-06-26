6251e37dae60f5a2622550b88433ae5b06058902
sahilkale2604 <157214195+sahilkale2604@users.noreply.github.com>
2024-06-26 07:48:56 +0000

    date and time

---
+++ basic/datetime.js
@@ -0,0 +1,7 @@
+let mydate =new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(typeof mydate);
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString('en-IN'));
+console.log(Math.round(mydate.getTime()/1000));
