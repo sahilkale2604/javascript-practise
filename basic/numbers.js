5af776e7a51af574bed176d6406d7f8be0115d2c
sahilkale2604 <157214195+sahilkale2604@users.noreply.github.com>
2024-06-26 07:34:02 +0000

    numbers and mats

---
+++ basic/numbers.js
@@ -0,0 +1,12 @@
+const othernumber = 100
+console.log(othernumber);
+console.log(typeof othernumber);
+console.log(othernumber.toString().length);
+console.log(othernumber.toFixed(2));
+console.log(othernumber.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++maths++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.floor(Math.random()*10)+1);
